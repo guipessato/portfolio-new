85571efc3458276c42691976f526daf66dcf2067
Guilherme Jose Pessato <guilherme.pessato@auaha.com.br>
2022-03-30 00:50:15 -0300

    up

---
+++ src/assets/snippets/projects/index.js
@@ -0,0 +1,14 @@
+$('.projects__list').slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    prevArrow: `<button type="button" aria-label="prev" class="slick-prev"><svg id="Grupo_478" data-name="Grupo 478" xmlns="http://www.w3.org/2000/svg" width="6" height="10.894" viewBox="0 0 6 10.894"><path id="Caminho_526" data-name="Caminho 526" d="M-368,146.255a.947.947,0,0,0-.286-.425q-2.145-2.141-4.288-4.284a1.058,1.058,0,0,0-.1-.079l-.019-.049a.5.5,0,0,0,.111-.064q2.149-2.145,4.3-4.291a.94.94,0,0,0,.286-.425v-.191a.642.642,0,0,0-.447-.447h-.213a1.087,1.087,0,0,0-.425.3q-2.3,2.3-4.606,4.6a1.088,1.088,0,0,0-.309.433v.213a1.1,1.1,0,0,0,.309.433q2.3,2.3,4.606,4.6a1.092,1.092,0,0,0,.425.3h.213a.647.647,0,0,0,.447-.447Z" transform="translate(374 -136)"/></svg></button>`,
+    nextArrow: `<button type="button" aria-label="next" class="slick-next"><svg id="Grupo_478" data-name="Grupo 478" xmlns="http://www.w3.org/2000/svg" width="6" height="10.894" viewBox="0 0 6 10.894"><path id="Caminho_526" data-name="Caminho 526" d="M-368,146.255a.947.947,0,0,0-.286-.425q-2.145-2.141-4.288-4.284a1.058,1.058,0,0,0-.1-.079l-.019-.049a.5.5,0,0,0,.111-.064q2.149-2.145,4.3-4.291a.94.94,0,0,0,.286-.425v-.191a.642.642,0,0,0-.447-.447h-.213a1.087,1.087,0,0,0-.425.3q-2.3,2.3-4.606,4.6a1.088,1.088,0,0,0-.309.433v.213a1.1,1.1,0,0,0,.309.433q2.3,2.3,4.606,4.6a1.092,1.092,0,0,0,.425.3h.213a.647.647,0,0,0,.447-.447Z" transform="translate(374 -136)"/></svg></button>`,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: 'unslick'
+        },
+    ]
+});
