e23ccb3812e325d716a810ef0985698ad2ac26e9
Guilherme Jose Pessato <guilherme.pessato@auaha.com.br>
2022-03-30 12:54:57 -0300

    up

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,16 @@ const st = ScrollReveal({
     duration: 1500,
 });
 
-st.reveal(".delaySmallReveal", { delay: 200 });
-st.reveal(".delayMediumReveal", { delay: 250 });
-st.reveal(".delayLargeReveal", { delay: 400 });
-st.reveal(".delayExtraBigReveal", { delay: 600 });
-st.reveal(".intervalCardReveal", { interval: 280 });
+if (jQuery(window).width() > 991) {
+    st.reveal(".delaySmallReveal", { delay: 200 });
+    st.reveal(".delayMediumReveal", { delay: 250 });
+    st.reveal(".delayLargeReveal", { delay: 400 });
+    st.reveal(".delayExtraBigReveal", { delay: 600 });
+    st.reveal(".intervalCardReveal", { interval: 280 });
+} else {
+    ScrollReveal().reveal('.projects__list');
+    ScrollReveal().reveal('.skills__cards');
+}
 
 ScrollReveal().reveal('.home__container');
 ScrollReveal().reveal('.about__container');
